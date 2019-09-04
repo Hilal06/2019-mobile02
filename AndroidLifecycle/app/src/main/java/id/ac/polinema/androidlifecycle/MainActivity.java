e9db982d8f39547484cabb815c54e1e40a1c24ff
Hilal06 <rifaulhilal06@gmail.com>
2019-09-04 17:16:08 +0700

    activity lifecycle

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -10,12 +10,36 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
 
+	}
 	// TODO: tambahkan callback onStart() di sini
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
 
 
 	// TODO: tambahkan callback onStop() di sini
 
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+
 	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
 }
